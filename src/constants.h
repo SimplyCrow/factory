4fc4997597016749f33e158fece958ce766157a2
Jonathan Kowalski <simplygreatcrow@outlook.com>
2026-06-08 09:05:53 +0200

    Export references to constants

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+extern const int WINDOW_WIDTH;
+extern const int WINDOW_HEIGHT;
+extern const int GAME_WIDTH;
+extern const int GAME_HEIGHT;
+extern const int FPS;
+extern const float METER_UNIT_SIZE;
+extern const float ITEM_SIZE;
+
+#endif // CONSTANTS_H
